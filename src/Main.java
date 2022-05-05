0fe0fb9c3e7822f244e3d7a53089f5f395248107
Андрей <andrews.ru83@gmail.com>
2022-05-05 20:10:25 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        String fullName = "Ivanov Ivan Ivanovich" ;
+        String fullNameBig = fullName.toUpperCase() ;
+        System.out.println(fullNameBig);
 
     }
 }
